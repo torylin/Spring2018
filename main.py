6b2898c2feb44ba3c85a81d0bd531ab00a0fc1f1
Victoria Lin <vlin2@andrew.cmu.edu>
2018-09-13 23:42:03 -0400

    Final

--- main.py
+++ main.py
@@ -24,18 +24,18 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     autoescape=True)
 
 # This API key is provided by google as described in the tutorial
-API_KEY = 'XXxxXxXXXXxxNXXxXXXxxxNNXXxxxxxxxXXXxXX'
+API_KEY = 'AIzaSyA8Mm7MAqyPOYCZ53xr_7M6BseiZcF0AEA'
 
 # This uses discovery to create an object that can talk to the 
 # fusion tables API using the developer key
 service = build('fusiontables', 'v1', developerKey=API_KEY)
 
 # This is the table id for the fusion table
-TABLE_ID = 'NxxxNXxXxxNxXXXXNXxXXXxXxxxNxXxNxXxxXxxX'
+TABLE_ID = '1YpWrzSFQ12dg531c0n8m8g4WsNG6SgEervUvdBGx'
 
 # This is the default columns for the query
 query_cols = []
-query_values = ['Forlan'] #Change to be the value(s) you're querying in the column you've specified
+query_values = ['2000', '2001', '2002'] #Change to be the value(s) you're querying in the column you've specified
 
 # Import the Flask Framework
 from flask import Flask, request
@@ -71,7 +71,7 @@ def make_query(cols, values, limit):
     string_values = string_values[2:len(string_values)]
     
     #Change this query to have your corresponding column (in our soccer example, the column for our WHERE is Scorer).
-    query = "SELECT " + string_cols + " FROM " + TABLE_ID + " WHERE Scorer = '" + string_values + "'"
+    query = "SELECT " + string_cols + " FROM " + TABLE_ID
 
     query = query + " LIMIT " + str(limit)
 
@@ -87,7 +87,7 @@ def make_query(cols, values, limit):
 def index():
     template = JINJA_ENVIRONMENT.get_template('templates/index.html')
     request = service.column().list(tableId=TABLE_ID)
-    res = get_all_data(make_query([], query_values, 5)) #5 is our limit we're passing in
+    res = get_all_data(make_query([], query_values, 200)) #5 is our limit we're passing in
     logging.info('allheaders')
     return template.render(columns=res['columns'], rows = res['rows'] )
 
@@ -99,7 +99,7 @@ def update_table():
     result = get_all_data(make_query(cols, query_values, 100))
     logging.info(result)
     return json.dumps({'content' : result['rows'], 'headers' : result['columns']})
-
+  
 @app.route('/about')
 def about():
     template = JINJA_ENVIRONMENT.get_template('templates/about.html')
